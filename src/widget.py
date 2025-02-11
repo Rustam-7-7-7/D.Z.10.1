bd5a7250809a6059af0ab6311daa65516e75b057
safin <safin@example.com>
2025-02-11 12:56:15 +0300

    add src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -13,7 +13,7 @@ def mask_account_card(type_and_number: Union[str]) -> Union[str]:
     digit_result = ""
     digit_count = 0
     for el in type_and_number:
-        if el.isalpha() or " ":
+        if el.isalpha():
             text_result += el
         elif el.isdigit():
             digit_result += el
