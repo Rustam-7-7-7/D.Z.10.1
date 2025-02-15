60d150c38c88f40f159ca49661f8a82813be503b
safin <safin@example.com>
2025-02-15 18:55:21 +0300

    add src/masks.py

--- src/masks.py
+++ src/masks.py
@@ -5,7 +5,7 @@ card_number = input()
 def get_mask_card_number(card_number: int) -> str:
     '''Функция, которая принимает на вход номер карты и возвращает ее маску.'''
 
-    return f"{card_number[:4]}{card_number[4:6]}** **** {card_number[12:]}"
+    return f"{card_number[:4]} {card_number[4:6]} ** **** {card_number[12:]}"
 
 print(get_mask_card_number(card_number(7000792289606361)))
 
