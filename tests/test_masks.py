81b29a8d9054a985e8bb39dea15e4d62f5fbd750
safin <safin@example.com>
2025-12-21 15:32:58 +0300

    homework 10_1 done

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,19 +1,20 @@
-from masks import get_mask_card_number, get_mask_account
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
 
 
 def test_get_mask_card_number():
     assert get_mask_card_number(7000792289606361) == 7000 79** **** 6361
 
-
-
-
+#Пример работы функции: 7000792289606361 # входной аргумент 7000 79** **** 6361 # выход функции
 
 
 
 def test_get_mask_account():
-    assert get_mask_account(2, 3) == 6
+    assert get_mask_account(73654108430135874305) == **4305
 
-
+#Пример работы функции: 73654108430135874305 # входной аргумент **4305 # выход функции
 
 
 
