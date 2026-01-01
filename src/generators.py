e972c1e1bffc475e8f8f1b1ccc556e651903b117
safin <safin@example.com>
2026-01-01 23:20:56 +0300

    homework 11_1 done

--- src/generators.py
+++ src/generators.py
@@ -103,7 +103,7 @@ transactions = (
 
 #Пример использования функции
 usd_transactions = filter_by_currency(transactions, "USD")
-for _ in range(2):
+for _ in range(3):
     print(next(usd_transactions))
 
 
@@ -142,5 +142,5 @@ def card_number_generator(start, end):
 
 
 #Пример использования функции
-for card_number in card_number_generator(1, 9999999999999999):
+for card_number in card_number_generator(1, 5):
     print(card_number)
