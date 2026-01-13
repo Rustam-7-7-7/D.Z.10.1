7e9a23404c7c85e88d7663de96dca25b17d9e405
safin <safin@example.com>
2026-01-13 16:52:44 +0300

    homework 12_2 done

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,14 @@
-# Домашнее задание № 12.1
+# Домашнее задание № 12.2
 #############################################################################
 
 
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: int, success=True) -> str:
     '''Функция, которая принимает на вход номер карты и возвращает ее маску.'''
+
+    if success:
+        logger.info('Function executed successfully.')
+    else:
+        logger.error('An error occurred in the function.')
 
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
@@ -12,5 +17,28 @@ def get_mask_account(account_number: int) -> str:
     '''Функция, которая принимает на вход номер счета и возвращает его маску.'''
 
     return f"**{account_number[-4:]}"
+
+
+# Логирование
+import logging
+
+logger = logging.getLogger('masks')
+
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('../logs/masks.log', mode='w')
+
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(file_handler)
+
+# Пример использования логера
+card_number = input(f'Введите номер карты: ')
+
+print(get_mask_card_number(card_number, success=True))
+
+print(get_mask_card_number(card_number, success=False))
 
 #############################################################################
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,15 @@
 import json
 
 
-def load_transactions(file_path):
+def load_transactions(file_path, success=True):
     ''' Функция, которая принимает на вход путь до JSON-файла и возвращает список
          словарей с данными о финансовых транзакциях. Если файл пустой, содержит не список
           или не найден, функция возвращает пустой список. '''
+    if success:
+        logger.info('Function executed successfully.')
+    else:
+        logger.error('An error occurred in the function.')
+
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
@@ -16,5 +21,28 @@ def load_transactions(file_path):
 
 
 # Пример использования функции
-transactions = load_transactions('../data/operations.json')
+# transactions = load_transactions('../data/operations.json')
+# print(transactions)
+
+
+# Логирование
+import logging
+
+logger = logging.getLogger('utils')
+
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('../logs/utils.log', mode='w')
+
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(file_handler)
+
+# Пример использования логера
+transactions = load_transactions('../data/operations.json', success=True)
+print(transactions)
+
+transactions = load_transactions('../data/operations.json', success=False)
 print(transactions)
